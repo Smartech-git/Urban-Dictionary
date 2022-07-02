256a368dae57d4ecf1e163ddf33193f5bd9444b5
Smartech-git <chukwusmartchukwuemeka@gmail.com>
2022-07-02 17:08:34 +0100

    intro page done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
+import Intro from './Intro';
+import {Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<Intro/>} />
+        <Route path='Home' element={<Home/>}  />
+      </Routes>
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home(props) {
+    return (
+        <div>
+           Home 
+        </div>
+    );
+}
+
+export default Home;
