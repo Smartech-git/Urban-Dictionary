20940129abc0394e621acda07b92ee84f29bcd04
Smartech-git <chukwusmartchukwuemeka@gmail.com>
2023-02-21 23:19:10 +0100

    added themeHue contexts

--- src/Reducer.js
+++ src/Reducer.js
@@ -1,14 +1,28 @@
+export const themeHueLight = {
+    primary: '#FFF',
+    primary_light: '#F3F5F7',
+    secondary_light: '#E5E5E5',
+}
+
+export const themeHueDark = {
+    primary: '#06010F',
+    primary_light: '#1F1A27',
+    secondary_light: '#C39CFC',
+}
 
 export const initialState = {
    wordContents: 'Default state',
    newContent: false,
    responseEmpty: false,
-   loading: false
+   loading: false,
+   theme: 'LIGHT',
+   themeHue: themeHueLight
 }
 
 export const actionTypes = {
    setWordContent: "SETWORDCONTENT",
-   setLoading: "SETLOADING"
+   setLoading: "SETLOADING",
+   setTheme: "SETTHEME"
 }
 
 const reducer = (state, action) => {
@@ -23,10 +37,18 @@ const reducer = (state, action) => {
             };
 
         case actionTypes.setLoading:
-        return {
-            ...state,
-             loading: action.loading
-        };        
+            return {
+                ...state,
+                loading: action.loading
+            };
+
+        case actionTypes.setTheme:
+            return {
+                ...state,
+                theme: action.theme,
+                themeHue: action.theme === 'LIGHT' ? themeHueLight : themeHueDark
+            };
+        
         default: 
             return state;
     }
