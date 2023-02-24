82bda30a49f5fe12d2549e327a8c8d219ac3192e
Smartech-git <chukwusmartchukwuemeka@gmail.com>
2023-02-24 20:02:39 +0100

    Added component for theme setting on mobile view

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Header from './Components/Header';
 import SearchBar from './Components/SearchBar';
 import Content from './Components/Content';
@@ -6,12 +6,48 @@ import NetworkErrorDisplay from './NetworkErrorDisplay';
 import { useStateValue } from './StateProvider';
 import HomeInfo from './HomeInfo';
 import LoadingDisplay from './LoadingDisplay';
+import { actionTypes } from './Reducer';
+import { getSettings } from './APIs';
+import { useThemeDetector } from './CustomHooks/useThemeDetector';
+import ThemeSetMobile from './Components/ThemeSetMobile/ThemeSetMobile';
 
 function Home(props) {
     const [state, dispatch] = useStateValue();
+    const isDarkTheme = useThemeDetector();
+
+    useEffect(() =>{
+        let settings = getSettings()
+        console.log(settings)
+        console.log(state)
+
+        if(settings.theme === "Auto"){
+
+            if(isDarkTheme){
+                const action = {
+                    type: actionTypes.setTheme,
+                    theme: 'Dark'
+                }
+                dispatch(action);
+            } else{
+                const action = {
+                    type: actionTypes.setTheme,
+                    theme: 'Light'
+                }
+                dispatch(action); 
+            }
+
+        } else {
+             const action = {
+                type: actionTypes.setTheme,
+                theme: settings.theme
+            }
+            dispatch(action);
+        }
+
+    }, [])
 
     return (
-        <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100vh'}}>
+        <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100%', boxSizing: 'border-box', backgroundColor: state.themeHue.primary}}>
             <Header/>
             <SearchBar/>
             {   
@@ -37,6 +73,7 @@ function Home(props) {
                     )
                 )
             }
+            <ThemeSetMobile/>
         </div>
     );
 }
